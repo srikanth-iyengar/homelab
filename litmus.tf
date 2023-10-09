00289cc2a578e5cd2d630d013a9f5dd715290872
Srikanth Iyengar <ksrikanth3012@gmail.com>
2023-10-09 22:11:48 +0530

    Bug/litmus sleep fix (#27)
    * the resource getting sleeped for litmus chaos deployment fixed
    
    * fixed the bug for updating the litmus thing so that it doesn't go to sleep
    Just a okteto thing

--- litmus.tf
+++ litmus.tf
@@ -1,9 +1,11 @@
+resource "time_static" "litmus_updated" {
+}
 resource "helm_release" "litmus-chaos" {
   name      = "litmuschart"
   chart     = "./charts/litmus"
   namespace = "observability-suite-srikanth-iyengar"
   values = [
-    "lastUpdated = ${timestamp()}",
-    "namespace =  observability-suite-srikanth-iyengar"
+    "namespace: observability-suite-srikanth-iyengar",
+    "lastUpdated: ab${time_static.litmus_updated.unix}"
   ]
 }
