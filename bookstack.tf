4f75f68f929b1ac466e20f7caa371d168d6741ba
Srikanth Iyengar <ksrikanth3012@gmail.com>
2023-12-10 16:50:00 +0530

    (feature) replaced bookstack broken deployment with codimd deployment (#34)

--- bookstack.tf
+++ bookstack.tf
@@ -1,5 +1,8 @@
 resource "random_string" "mysql_password" {
   length = 35
+  special = false
+  upper = false
+  numeric = false
 }
 
 resource "random_string" "mysql_volume_id" {
@@ -14,100 +17,6 @@ resource "random_string" "bookstack_volume" {
   special = false
   upper   = false
   numeric = false
-}
-
-resource "kubernetes_deployment" "bookstack_deployment" {
-  metadata {
-    name      = "bookstack-deployment"
-    namespace = "productivity-stack-srikanth-iyengar"
-  }
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        App = "bookstack-deployment"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "bookstack-deployment"
-        }
-      }
-      spec {
-        init_container {
-          name              = "init-master"
-          image             = "busybox"
-          image_pull_policy = "Always"
-
-          command = ["sh", "-c", "chmod 777 /volumes/*"]
-
-          volume_mount {
-            mount_path = "/volumes/${random_string.bookstack_volume.result}"
-            name       = random_string.bookstack_volume.result
-          }
-        }
-        init_container {
-          name              = "init-volume-master"
-          image             = "solidnerd/bookstack"
-          image_pull_policy = "IfNotPresent"
-
-          command = ["sh", "-c", "echo initializing volume... && (cp -Rv /var/www/bookstack/. /init-volume-0 || true)"]
-
-          volume_mount {
-            mount_path = "/init-volume-0"
-            name       = random_string.bookstack_volume.result
-            sub_path   = random_string.bookstack_volume.result
-          }
-        }
-        container {
-          image = "solidnerd/bookstack"
-          name  = "bookstack"
-          volume_mount {
-            name       = random_string.bookstack_volume.result
-            mount_path = "/var/www/bookstack"
-            sub_path   = random_string.bookstack_volume.result
-          }
-          resources {
-            limits = {
-              cpu    = "1"
-              memory = "1024Mi"
-            }
-          }
-          env {
-            name  = "DB_HOST"
-            value = "mysql-service.productivity-stack-srikanth-iyengar.svc.cluster.local:3306"
-          }
-          env {
-            name  = "DB_DATABASE"
-            value = "bookstack-db"
-          }
-          env {
-            name  = "DB_USERNAME"
-            value = "root"
-          }
-          env {
-            name  = "DB_PASSWORD"
-            value = random_string.mysql_password.result
-          }
-          env {
-            name  = "APP_URL"
-            value = "https://bookstack.srikanthk.in"
-          }
-          env {
-            name  = "APP_KEY"
-            value = "https://bookstack.srikanthk.in"
-          }
-        }
-        volume {
-          name = random_string.bookstack_volume.result
-          persistent_volume_claim {
-            claim_name = random_string.bookstack_volume.result
-          }
-        }
-      }
-    }
-  }
 }
 
 resource "kubernetes_deployment" "mysql_deployment" {
@@ -178,25 +87,6 @@ resource "kubernetes_deployment" "mysql_deployment" {
   }
 }
 
-resource "kubernetes_service" "bookstack_service" {
-  metadata {
-    name      = "bookstack-service"
-    namespace = "productivity-stack-srikanth-iyengar"
-  }
-
-  spec {
-    selector = {
-      App = "bookstack-deployment"
-    }
-    port {
-      name        = "bookstack-service"
-      target_port = 8080
-      port        = 8080
-      protocol    = "TCP"
-    }
-  }
-}
-
 resource "kubernetes_service" "mysql_service" {
   metadata {
     name      = "mysql-service"
@@ -227,23 +117,6 @@ resource "kubernetes_persistent_volume_claim" "bookstack_pvc" {
     resources {
       requests = {
         storage = "1Gi"
-      }
-    }
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "bookstack_storage_pvc" {
-  metadata {
-    name      = random_string.bookstack_volume.result
-    namespace = "productivity-stack-srikanth-iyengar"
-  }
-
-  spec {
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "okteto-standard"
-    resources {
-      requests = {
-        storage = "1512Mi"
       }
     }
   }
---
+++ codimd.tf
@@ -0,0 +1,112 @@
+resource "kubernetes_service" "codimd_service" {
+  metadata {
+    name      = "codimd-svc"
+    namespace = "productivity-stack-srikanth-iyengar"
+  }
+  spec {
+    type                        = "ClusterIP"
+    publish_not_ready_addresses = true
+    port {
+      name        = "codimd-port"
+      port        = "3000"
+      target_port = "3000"
+    }
+    selector = {
+      App = "codimd-sts"
+    }
+  }
+}
+
+resource "kubernetes_config_map" "codimd_entrypoint" {
+  metadata {
+    name      = "codimd-startscript"
+    namespace = "productivity-stack-srikanth-iyengar"
+    labels = {
+      conf = "codimd"
+    }
+  }
+  data = {
+    "start.sh" = <<EOF
+    #!/usr/bin/env bash
+
+set -euo pipefail
+
+npm install pg --save
+
+if [[ "$#" -gt 0 ]]; then
+    exec "$@"
+    exit $?
+fi
+
+# check database and redis is ready
+pcheck -env CMD_DB_URL
+
+# run DB migrate
+NEED_MIGRATE=`CMD_AUTO_MIGRATE:=true`
+
+if [[ "$NEED_MIGRATE" = "true" ]] && [[ -f .sequelizerc ]] ; then
+    npx sequelize db:migrate
+fi
+
+# start application
+node app.js
+EOF
+  }
+}
+
+resource "kubernetes_stateful_set" "codimd_sts" {
+  metadata {
+    name      = "codimd-sts"
+    namespace = "productivity-stack-srikanth-iyengar"
+  }
+  spec {
+    service_name          = "codimd-svc"
+    pod_management_policy = "OrderedReady"
+    replicas              = 1
+    selector {
+      match_labels = {
+        App = "codimd-sts"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "codimd-sts"
+        }
+      }
+      spec {
+        security_context {
+          fs_group = 1001
+        }
+        container {
+          name  = "codimd-container"
+          image = "hackmdio/hackmd"
+          security_context {
+            run_as_non_root            = true
+            run_as_user                = 1001
+            allow_privilege_escalation = false
+          }
+          env {
+            name  = "CMD_DB_URL"
+            value = "mysql://root:${random_string.mysql_password.result}@mysql-service.productivity-stack-srikanth-iyengar.svc.cluster.local:3306/codimd"
+            # value = random_string.mysql_password.result
+          }
+        }
+      }
+    }
+    volume_claim_template {
+      metadata {
+        name      = "datadir"
+        namespace = "productivity-stack-srikanth-iyengar"
+      }
+      spec {
+        access_modes = ["ReadWriteOnce"]
+        resources {
+          requests = {
+            storage = "1024Mi"
+          }
+        }
+      }
+    }
+  }
+}
